ea73ab41462e10d2785d686dff993b1d80742073
Marcos <mail1.aluno.ifal.edu.br>
2021-05-25 10:42:46 -0300

    Integration of the CountryCard widget and the RestCountries class in HomePage screen

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:country_search/services/rest_countries.dart';
+import 'package:country_search/home/widgets/country_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +10,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> countryInfo = [];
+
+  void _restCountriesSetup() async {
+    RestCountries instance = RestCountries();
+    await instance.getCountries();
+
+    setState(() {
+      countryInfo = instance.countries;
+    });
+  }
+
   @override
   void initState() {
+    _restCountriesSetup();
     super.initState();
   }
 
@@ -20,8 +34,29 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Países"),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+              },
+              child: Icon(
+                Icons.search,
+                size: 26.0,
+              ),
+            ),
+          )
+        ],
       ),
       body: Container(
+        child: ListView.builder(
+          itemCount: countryInfo.length,
+          itemBuilder: (context, index) {
+            return CountryCard(
+              country: countryInfo[index],
+            );
+          },
+        )
       ),
     );
   }
